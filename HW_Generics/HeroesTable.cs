14c67c8f57abace1c20c040e7db076c8683ca75f
Shiran Yitzhak <shiran.yitzhak@gmail.com>
2022-03-17 15:47:27 +0200

    Add project files.

---
+++ HW_Generics/HeroesTable.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_Generics
+{
+    internal class HeroesTable<T> where T : Heroes
+    {
+        public T[] Knights { get; set; }
+
+        public T this[int index]
+        {
+            get
+            {
+                if (ActualKnights() == 0)
+                    return default (T);
+                else if (index <= Knights.Length - 1)
+                {
+                    if (Knights[index] == null)
+                    {
+                        for (int i = index - 1; i >= 0; i--)
+                        {
+                            if (Knights[i] != null)
+                                return Knights[i];
+                        }
+                        return default(T);
+                    }
+                    else
+                        return Knights[index];
+                }
+                else
+                {
+                    return Knights[index % Knights.Length-1];
+                }
+            }
+            set
+            {
+
+            }
+        }
+
+        public HeroesTable(int size)
+        {
+            Knights = new T[size];
+        }
+
+        public void Set(T knight, int index)
+        {
+            Knights[index] = knight;
+        }
+
+        public void Delete(int index)
+        {
+            Knights[index] = default(T);
+        }
+
+        public int ActualKnights()
+        {
+            int counter = 0;
+            foreach (T knight in Knights)
+            {
+                if (knight != null)
+                    counter++;
+            }
+            return counter;
+        }
+        public T[] AllKnights()
+        {
+            T[] allKnights = new T[ActualKnights()];
+            int index = 0;
+            foreach (T knight in Knights)
+            {
+                if (knight != null)
+                {
+                    allKnights[index] = knight;
+                    index++;
+                }
+            }
+            return allKnights;
+        }
+    }
+
+}
---
+++ HW_Generics/Program.cs
@@ -0,0 +1,27 @@
+﻿
+
+using System;
+
+namespace HW_Generics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HeroesTable<Knight> ht1 = new HeroesTable<Knight>(5);
+            Knight k1 = new Knight();
+            Knight k2 = new Knight();
+            ht1.Set(k1, 0);
+            ht1.Set(k2, 2);
+            Knight k3 = ht1[2];
+
+            HeroesTable<Sword> ht2 = new HeroesTable<Sword>(6);
+            Sword s1 = new Sword();
+            Sword s2 = new Sword();
+            ht2.Set(s1, 1);
+            ht2.Set(s2, 3);
+            Sword s3 = ht2[4];
+        }
+    }
+}
+
